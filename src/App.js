f94d06aaf9eab5f16dd1f8c4ecce307eaf806072
Heather Frantisak <hfrantisak@mvnp.com>
2017-06-09 12:21:35 -1000

    Handle click event, to show posts from clicked news source. Create component to handle all posts.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Post from './Post';
+import Posts from './Posts';
 
 import './App.css';
 
@@ -11,46 +11,45 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      posts: [],
+      url: '',
+      visible: false,
     }
-  }
-
-  componentDidMount() {
-    urls.forEach((value, key) => {
-      // TODO use key for headings, selecting/navigating to different sources
-      this.fetchPosts(value.concat('/wp-json/wp/v2/posts'))
-    })
-  }
-
-  fetchPosts(url) {
-    return fetch(url)
-            .then((response) => response.json())
-            .then((json) => {
-              this.setState({
-                posts: json
-              })
-            })
-            .catch((error) => {
-              console.error(`Error in response: ${error}`)
-            })
+    this.showPosts = this.showPosts.bind(this)
   }
 
   showSources() {
     let sourceArray = []
     urls.forEach((v, k) => {
-      sourceArray.push(<li key={k}>{k}</li>)
+      sourceArray.push(
+        <li key={k}>
+          Source: {k}
+          <button id={k} onClick={(e) => this.showPosts(e, v)}>Show posts</button>
+        </li>
+      )
     })
     return sourceArray
   }
 
+  showPosts(e, valueUrl) {
+    e.preventDefault()
+    this.setState({
+      url: valueUrl,
+      visible: true,
+    })
+  }
+
   render() {
-    const sourceList = urls.forEach((v, k) => <li>{k}</li>)
     const sourceNames = this.showSources()
 
     return (
       <div className="App">
         <h1>News sources available</h1>
           <ul>{sourceNames}</ul>
+          {
+            this.state.visible ?
+            <Posts url={this.state.url} /> :
+            <span>Click news source</span>
+          }
       </div>
     );
   }
---
+++ src/Posts.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+import Post from './Post'
+
+class Posts extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      posts: [],
+    }
+  }
+
+  componentDidMount() {
+    this.fetchPosts(this.props.url.concat('/wp-json/wp/v2/posts'))
+  }
+
+  fetchPosts(url) {
+    return fetch(url)
+            .then((response) => response.json())
+            .then((json) => {
+              this.setState({
+                posts: json
+              })
+            })
+            .catch((error) => {
+              console.error(`Error in response: ${error}`)
+            })
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.posts.map((value, key) => {
+          return (
+            <Post
+              key={key}
+              title={value.title.rendered}
+              content={value.content.rendered}
+            />
+          )
+        })}
+      </div>
+    )
+  }
+}
+
+export default Posts;
